1c70ec25e1acdcc11325c63e70ead8bedafd606a
misogihagi <47350059+misogihagi@users.noreply.github.com>
2025-03-11 03:57:29 +0900

    format

--- library/tplink.py
+++ library/tplink.py
@@ -7,53 +7,50 @@ except ImportError:
     from module_utils.RouterConfig import RouterConfig
 
 
-
 def login(page, url, username, password):
-      page.goto(url)
-      page.get_by_placeholder("ユーザー名かパスワードが違います").click()
-      page.get_by_placeholder("ユーザー名かパスワードが違います").fill(username)
-      page.get_by_placeholder("ユーザー名かパスワードが違います").press("Tab")
-      page.get_by_placeholder("パスワード", exact=True).fill(password)
-      page.get_by_placeholder("パスワード", exact=True).press("Enter")
-      expect(page.locator("frame[name=\"topFrame\"]")).to_be_visible()
-      return page
+    page.goto(url)
+    page.get_by_placeholder("ユーザー名かパスワードが違います").click()
+    page.get_by_placeholder("ユーザー名かパスワードが違います").fill(username)
+    page.get_by_placeholder("ユーザー名かパスワードが違います").press("Tab")
+    page.get_by_placeholder("パスワード", exact=True).fill(password)
+    page.get_by_placeholder("パスワード", exact=True).press("Enter")
+    return page
 
 
 def run(url, username, password, configuration):
-     with sync_playwright() as p:
-      browser = p.chromium.launch(headless=False)
-      context = browser.new_context()
-      page = context.new_page()
-      config = RouterConfig(**configuration)
-      print(config)
-      page = login(page, url, username, password)
-      context.close()
-      browser.close()
- 
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page = login(page, url, username, password)
+        config = RouterConfig(**configuration)
+        config.run(page, username=username, password=password)
+        context.close()
+        browser.close()
+
 
 def main():
     module_args = dict(
-        url=dict(type='str', required=True),
-        username=dict(type='str', required=True),
-        password=dict(type='str', required=True, no_log=True),
-        configuration=dict(type='dict', required=True),
+        url=dict(type="str", required=True),
+        username=dict(type="str", required=True),
+        password=dict(type="str", required=True, no_log=True),
+        configuration=dict(type="dict", required=True),
     )
-
 
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
 
-    url = module.params['url']
-    username = module.params['username']
-    password = module.params['password']
-    configuration = module.params['configuration']
+    url = module.params["url"]
+    username = module.params["username"]
+    password = module.params["password"]
+    configuration = module.params["configuration"]
 
     try:
-      run(url, username, password, configuration)
-      module.exit_json(changed=False)
+        run(url, username, password, configuration)
+        module.exit_json(changed=False)
 
     except Exception as e:
         module.fail_json(msg=str(e))
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
